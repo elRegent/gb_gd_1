53399341c0d94be87842f2ef199e5ca6e03587f9
elRegent <el.regent@gmail.com>
2020-06-01 19:42:58 +0300

    feat(hw): домашка 2ой лекции про управляющие конструкции

---
+++ 02_Lesson/FunnyRecursiveNumbers/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).
+// б) * Разработать рекурсивный метод, который считает сумму чисел от a до b.
+namespace FunnyRecursiveNumbers
+{
+    class Program
+    {
+        static void printNumbers(int from, int to)
+        {
+            if (from > to)
+            {
+                return;
+            }
+
+            Console.Write($"{from} ");
+            printNumbers(from + 1, to);
+        }
+
+        static int sumNumbers(int from, int to, int sum = 0)
+        {
+            if (from > to)
+            {
+                return sum;
+            }
+
+            int sumWithNew = from + sum;
+
+            return sumNumbers(from + 1, to, sumWithNew);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите программу a или b (латинскими)");
+            string selectedProgram = Console.ReadLine();
+
+            Console.WriteLine("Введите число с которого начинаем");
+            int from = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число на котором заканчиваем");
+            int to = int.Parse(Console.ReadLine());
+
+
+            if (selectedProgram == "a")
+            {
+                Console.WriteLine("Результат работы программы a");
+                printNumbers(from, to);
+            }
+            else if (selectedProgram == "b")
+            {
+                int result = sumNumbers(from, to);
+                Console.WriteLine("Результат работы программы b");
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Программа еще не написана");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_Lesson/MinFromThree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Написать метод, возвращающий минимальное из трёх чисел.
+
+namespace MinFromThree
+{
+    class Program
+    {
+        static int FindMin(int a, int b, int c)
+        {
+            return (a < b && a < c) ? a : (b < c ? b : c);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите первое число");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите первое число");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Минимум равен {FindMin(a, b, c)}");
+
+            Console.ReadKey();
+        }
+    }
+}
